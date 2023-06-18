15fa167c0b258b2931f6b76da22a6c305445ecb0
Neto test <zilya.zakirova1991@gmail.com>
2023-06-18 18:19:54 +0300

    Fixed code

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,9 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
+        //      if (amount==1000){
+//            return 0;
+//        }else
         return boundary - amount % boundary;
     }
 }
